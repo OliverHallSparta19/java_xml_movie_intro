5c0efa610ad2ab6973aded2426feefe423e82c40
Oliver <OHall@spartaglobal.com>
2018-11-06 10:44:45 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+This maven project is still in development
+
+This will be a video player app
+
+Developed by Oliver Hall
---
+++ src/main/java/java_xml_movie_intro/App.java
@@ -0,0 +1,13 @@
+package java_xml_movie_intro;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
